6001cda42e6ee7c34533ca7daa36cb3f1c2b8df3
James Britton <brittonjp@gmail.com>
2019-06-14 10:40:20 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,15 @@ class Patient
   end
   
   def new_appointment(date, doctor)
-    Appointment.new
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select { |appointment| appointment.patient == self }
+  end
+  
+  def doctors
+    self.appointments.collect { |appointment| appointment.doctor }
   end
   
 end
