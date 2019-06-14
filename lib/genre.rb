4754f6e19411008baae90a6da071313af5d36924
James Britton <brittonjp@gmail.com>
2019-06-14 10:20:43 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,24 @@
 class Genre
   
+  attr_accessor :name, :artist, :genre
   
   @@all = []
   
   def initialize(name)
     @name = name
     @@all << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    self.class.all.select { |song| song.genre == self }
+  end
+  
+  def artists
+    self.songs.collect { |song| song.artist }
   end
   
 end
