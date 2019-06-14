cbaac38553d6420cce8381ed3227cfc3944eb979
James Britton <brittonjp@gmail.com>
2019-06-14 10:32:07 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,25 @@
+class Doctor
+  
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(patient, date)
+    appointment = Appointment.new(date)
+    appointment.doctor = self
+    appointment.patient = patient
+  end
+  
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+  
+end
